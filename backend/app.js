df261dfeb98eee6382a71ded87b80afb0064d151
MadhawaNethsara <madhawa.nethsara24@gmail.com>
2026-05-06 21:40:40 +0530

    fix app.js

--- backend/app.js
+++ backend/app.js
@@ -13,14 +13,14 @@ app.use('/api/salespeople', salespersonRouter);
 
 app.use('/api', router);
 
-app.use(
-    cors({
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowHeaders: ["Content-Type", "Authorization"],
-        credentials: true, 
-    })
-)
+// app.use(
+//     cors({
+//         origin: "*",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         allowHeaders: ["Content-Type", "Authorization"],
+//         credentials: true, 
+//     })
+// )
 
 app.use(notFound);
 app.use(errorHandler);
